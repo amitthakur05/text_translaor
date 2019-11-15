57c5167d5580eb84adc750104dadffa826164c81
amit <amit.gla_mca18@gla.ac.in>
2019-11-15 09:59:22 +0530

    image adding in imageview

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class Main2Activity extends AppCompatActivity {
+ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        imageView=findViewById(R.id.imageView);
+        Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cam,123);
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 123 && resultCode == RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,30 +21,32 @@ import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    EditText e;
-    TextView view;
+    Button translate,add_image;
+    EditText enter_text;
+    TextView view_for_tanslatedtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.button);
-        e=findViewById(R.id.editText);
-        view=findViewById(R.id.textView);
+        translate = findViewById(R.id.button);
+        enter_text=findViewById(R.id.editText);
+        view_for_tanslatedtext=findViewById(R.id.textView);
+        add_image=findViewById(R.id.button2);
 
-        //language download
+        //language configuration
+
         FirebaseTranslatorOptions options =
                 new FirebaseTranslatorOptions.Builder()
                         .setSourceLanguage(FirebaseTranslateLanguage.EN)
                         .setTargetLanguage(FirebaseTranslateLanguage.HI)
                         .build();
-        final FirebaseTranslator englishHindiTranslator =
+        final FirebaseTranslator LanguageTranslator =
                 FirebaseNaturalLanguage.getInstance().getTranslator(options);
 
         FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
                 .build();
-        englishHindiTranslator.downloadModelIfNeeded(conditions)
+        LanguageTranslator.downloadModelIfNeeded(conditions)
                 .addOnSuccessListener(
                         new OnSuccessListener<Void>() {
                             @Override
@@ -60,30 +62,37 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
 
-btn.setOnClickListener(new View.OnClickListener() {
+        //translating language
+translate.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        String text=e.getText().toString();
-        englishHindiTranslator.translate(text)
+        String text=enter_text.getText().toString();
+        LanguageTranslator.translate(text)
                 .addOnSuccessListener(
                         new OnSuccessListener<String>() {
                             @Override
                             public void onSuccess(@NonNull String translatedText) {
-                                view.setText(translatedText);
+                                view_for_tanslatedtext.setText(translatedText);
                             }
                         })
                 .addOnFailureListener(
                         new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this,"Try Again!!",Toast.LENGTH_SHORT).show();
 
                             }
                         });
-
-
-    }
-});
-
+                }
+        });
+// accessing text from an image
+        add_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
